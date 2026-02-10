e2fd1c14c9a799852070fd3bba247199386df35f
vibhatsu <maulikbarot2915@gmail.com>
2026-02-11 02:19:58 +0530

    refactor(passes): update pipeline with new passes

--- crates/slicer-passes/src/lib.rs
+++ crates/slicer-passes/src/lib.rs
@@ -1,16 +1,24 @@
+mod argument_removal;
 mod clangd;
 mod dead_code;
 mod dead_function;
+mod enum_struct_removal;
+mod global_removal;
 mod header_removal;
 mod statement_merge;
+mod struct_member_removal;
 mod typedef;
 mod unused_variable;
 mod util;
 
+pub use argument_removal::ArgumentRemovalPass;
 pub use dead_code::DeadCodePass;
 pub use dead_function::DeadFunctionPass;
+pub use enum_struct_removal::EnumStructRemovalPass;
+pub use global_removal::GlobalRemovalPass;
 pub use header_removal::HeaderRemovalPass;
 pub use statement_merge::StatementMergePass;
+pub use struct_member_removal::StructMemberRemovalPass;
 pub use typedef::TypedefPass;
 pub use unused_variable::UnusedVariablePass;
 
@@ -21,8 +29,12 @@ pub fn all_passes() -> Vec<Box<dyn ReductionPass>> {
         Box::new(DeadFunctionPass),
         Box::new(DeadCodePass),
         Box::new(UnusedVariablePass),
+        Box::new(ArgumentRemovalPass),
+        Box::new(GlobalRemovalPass),
+        Box::new(StructMemberRemovalPass),
         Box::new(StatementMergePass),
         Box::new(TypedefPass),
+        Box::new(EnumStructRemovalPass),
         Box::new(HeaderRemovalPass),
     ]
 }
